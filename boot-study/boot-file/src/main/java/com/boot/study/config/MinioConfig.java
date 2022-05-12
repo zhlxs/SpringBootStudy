1aeafa77c83370bb2bac7fb7438a069bdf295d11
Hsl <1358311815@qq.com>
2022-05-12 08:34:21 +0800

    add:文件模块

---
+++ boot-study/boot-file/src/main/java/com/boot/study/config/MinioConfig.java
@@ -0,0 +1,27 @@
+package com.boot.study.config;
+
+import com.boot.study.util.MinioUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.endpoint}")
+    private String endpoint;
+    @Value("${minio.bucketName}")
+    private String bucketName;
+    @Value("${minio.accessKey}")
+    private String accessKey;
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public MinioUtils creatMinioClient() {
+        return new MinioUtils(endpoint, bucketName, accessKey, secretKey);
+    }
+
+}
---
+++ boot-study/boot-file/src/main/java/com/boot/study/config/MinioProperties.java
@@ -0,0 +1,16 @@
+package com.boot.study.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "minio")
+public class MinioProperties {
+
+    private String endpoint;
+    private String accessKey;
+    private String secretKey;
+    private String bucketName;
+}
